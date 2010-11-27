00362c5f0a85e2fdd6bea389e92e3f15dc2b9c15
Giles Thomas <giles@giles.net>
2010-11-27 01:27:54 +0000

    Continued refactoring

--- audio.js
+++ audio.js
@@ -14,7 +14,7 @@ function Voice(oscillator, attack, decay, sustain, release) {
 
 
 // Developer's heuristic -- if you call something an XXManager, you don't really understand what it does...
-function AudioManagerForWantOfABetterName() {
+function AudioManagerForWantOfABetterName(getAmplitude) {
     this.sampleRate = 44100;
     var portionSize = this.sampleRate / 100;
     var prebufferSize = this.sampleRate / 4;
